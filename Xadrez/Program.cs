56a8c59216769de43429a761765f8348572e6cc0
Vinicius Oliveira <vinicius_correa@outlook.com>
2021-09-09 16:00:44 -0300

    Melhorando a implementação do metodo imprimirTabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -9,23 +9,15 @@ namespace Xadrez
         static void Main(string[] args)
         {
 
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-
-            Console.WriteLine(pos);
-
-            Console.WriteLine(pos.toPosicao());
-
-
-            Console.ReadLine();
-
-
-            /*
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+               
 
                 Tela.imprimirTabuleiro(tab);
 
@@ -36,7 +28,7 @@ namespace Xadrez
             }
 
             Console.ReadLine();
-            */
+            
 
         }
     }
--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -11,6 +11,8 @@ namespace Xadrez
         {   //Populando tabuleiro
             for(int i=0; i<tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
+
                 for(int j=0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -19,7 +21,8 @@ namespace Xadrez
                     }
                     else
                     {   //Endereçamento de peça no tabuleiro
-                        Console.Write(tab.peca(i, j) + " ");
+                        Tela.imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                     
                 }
@@ -27,11 +30,27 @@ namespace Xadrez
                 //Quebra de linha para formato de tabuleiro
                 Console.WriteLine();
             }
-            
-        
+
+            Console.WriteLine("  a b c d e f g h");
 
         }
+            
+        //Definindo a cor ao imprimir a peça
+            public static void imprimirPeca(Peca peca)
+        {   
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
 
+        }
 
     }
 }
